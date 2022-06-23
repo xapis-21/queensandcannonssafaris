798ecacaa910b1e2e9e4a53484c268aa8d8087fa
xapis-21 <78486475+xapis-21@users.noreply.github.com>
2022-06-23 04:03:29 +0300

    My task list project completed

---
+++ backend/qc/schemas/about.js
@@ -0,0 +1,22 @@
+export default {
+  name: "about",
+  title: "About",
+  type: "document",
+  fields: [
+    {
+      name: "about",
+      title: "About Queens and cannons",
+      type: "string",
+    },
+    {
+      name: "email",
+      title: "Email",
+      type: "string",
+    },
+    {
+      name: "phone",
+      title: "Phone Number",
+      type: "string",
+    },
+  ],
+};
